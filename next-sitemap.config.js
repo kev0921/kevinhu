9fc71f26a50c7b291b6fa401009913cef8dc8b41
kev0921 <kevin.hu0921@gmail.com>
2024-12-26 12:52:45 -0500

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://kevinhu04.netlify.app/", // Replace with your website URL
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
