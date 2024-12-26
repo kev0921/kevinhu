e9cbbcc7ea7fde1bc6aadf449f68850623eccb9c
kev0921 <kevin.hu0921@gmail.com>
2024-12-26 14:23:55 -0500

    updated top github repos list

--- src/constant/getGithubRepos.ts
+++ src/constant/getGithubRepos.ts
@@ -7,12 +7,12 @@ const getGithubRepos = async (username: string) => {
     );
 
     let repos = res.data;
-    let latestSixRepos = repos.splice(0, 8);
-    return latestSixRepos;
-    // let result = repos
-    //   ?.sort((a: any, b: any) => b.stargazers_count - a.stargazers_count)
-    //   .slice(0, 8);
-    // return result;
+    // let latestSixRepos = repos.splice(0, 8);
+    // return latestSixRepos;
+    let result = repos
+      ?.sort((a: any, b: any) => b.stargazers_count - a.stargazers_count)
+      .slice(0, 6);
+    return result;
   } catch (err) {
     console.log(err);
   }
